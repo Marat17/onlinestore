200151bbcce85438ec1f2a206264ef0d577301d8
Marat17 <muhutdinov@hotmail.com>
2018-08-01 20:40:52 +0300

    Sorry, big commit. Created 5 models, 1 view, urls, changed admin, added first product. Index page works. Niiice.

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
-# Create your models here.
+class Category(models.Model):
+    c_name = models.CharField(max_length=80)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.c_name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+
+class Product(models.Model):
+    p_name = models.CharField(max_length=80)
+    p_price = models.IntegerField()
+    p_description = models.TextField(max_length=140)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.p_name
+
+
+class ElementInCart(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    amount = models.IntegerField(default=1)
+
+
+class Cart(models.Model):
+    number_of_products = models.IntegerField(default=0)
+    elements = models.ManyToManyField(ElementInCart)
+    url = models.URLField()
+
+    def addelement(self, product, amount):
+        self.elements += product
+        self.number_of_products += amount
+
+
+class User(models.Model):
+    name = models.CharField(max_length=25)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    element = models.ForeignKey(ElementInCart, on_delete=models.CASCADE)
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Product
 
+def index(request):
+    allproducts = Product.objects.order_by('p_price')
+    context = {
+        'allproducts': allproducts
+    }
+    return render(request, 'shop/index.html', context)
 # Create your views here.
