8562b30a8373b62fb12de6edd3f1e4809d08de71
Marat17 <muhutdinov@hotmail.com>
2018-08-22 22:09:19 +0300

    Checkout page works. Magic.

--- onlinestore/urls.py
+++ onlinestore/urls.py
@@ -13,12 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+from django.conf.urls import url
 from django.urls import path, include
 from shop import views
+from django.contrib import admin
+
 
 urlpatterns = [
     path('', views.index, name='mainpage'),
     path('shop/', include('shop.urls')),
     path('admin/', admin.site.urls),
+    url(r'^orders/', include(('orders.urls', 'orders'), namespace='orders')),
 ]
