8ac071d885d1c080e2758414252b583a995b3ad2
Marat17 <muhutdinov@hotmail.com>
2018-08-30 17:51:50 +0300

    trying to make heroku work

--- onlinestore/settings.py
+++ onlinestore/settings.py
@@ -130,7 +130,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    '/shop/static/shop/',
+    '/WorkSpace/onlinestore/shop/static/shop/',
 ]
 
 CART_SESSION_ID = 'cart'
