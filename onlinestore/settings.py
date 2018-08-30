d8e6c453d32b6d5888678d58a2c01aeda6cab83a
Marat17 <muhutdinov@hotmail.com>
2018-08-30 22:03:35 +0300

    Debug false

--- onlinestore/settings.py
+++ onlinestore/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 't922r3)6b603!6xheb9ii0fo%k*8!)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'afternoon-depths-90136.herokuapp.com',
