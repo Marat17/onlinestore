a93be5af1a026c6a873814133b27f4245f5ca08f
Marat17 <muhutdinov@hotmail.com>
2018-08-30 17:44:54 +0300

    trying to make heroku work

--- onlinestore/settings.py
+++ onlinestore/settings.py
@@ -126,7 +126,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/shop/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/shop/static/shop/',
+]
 
 CART_SESSION_ID = 'cart'
 
