6fbfc1318d4fe462bf3e0320f785782c2a3418e3
Marat17 <muhutdinov@hotmail.com>
2018-09-07 00:30:22 +0300

    debug = false

--- onlinestore/settings.py
+++ onlinestore/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 't922r3)6b603!6xheb9ii0fo%k*8!)nph(h7%2k+wudq8hjdgf')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'afternoon-depths-90136.herokuapp.com',
