2c750e3615b1963d9530af8f840ef10884f83ecd
Marat17 <muhutdinov@hotmail.com>
2018-09-07 00:08:40 +0300

    +

--- onlinestore/urls.py
+++ onlinestore/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^orders/', include(('orders.urls', 'orders'), namespace='orders')),
     path('shop/', include('shop.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
