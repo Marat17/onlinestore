cfcb1f8eabacdc861a3bf288e295a17b55f3a063
Marat17 <muhutdinov@hotmail.com>
2018-09-09 23:44:35 +0300

    Celery on Heroku

--- onlinestore/settings.py
+++ onlinestore/settings.py
@@ -146,8 +146,8 @@ EMAIL_USE_TLS = True
 
 #Heroku and Celery
 import os
-app.conf.update(BROKER_URL=os.environ['REDIS_URL'],
-                CELERY_RESULT_BACKEND=os.environ['REDIS_URL'])
+BROKER_URL=os.environ['REDIS_URL'],
+CELERY_RESULT_BACKEND=os.environ['REDIS_URL']),
 
 #PayPal
 PAYPAL_RECEIVER_EMAIL = 'muhutdinov@hotmail.com'
