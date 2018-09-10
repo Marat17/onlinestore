1d8137561156cf186b2b25aea8e895e9f80de6c9
Marat17 <muhutdinov@hotmail.com>
2018-09-10 22:41:37 +0300

    make Celery work

--- onlinestore/settings.py
+++ onlinestore/settings.py
@@ -157,7 +157,7 @@ app.conf.update(BROKER_URL=os.environ['REDIS_URL'],
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = os.environ['REDIS_PORT']
 EMAIL_HOST_USER = "celerytest123@gmail.com"
-EMAIL_HOST_PASSWORD = "Celery123"
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
 EMAIL_USE_TLS = True
 
 import redis
